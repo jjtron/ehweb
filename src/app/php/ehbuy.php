0a86e7a8118219e47181180e524902452178fdc9
jjtron <gpetron4@cfl.rr.com>
2018-10-22 20:29:28 -0400

    Jazz up the Pay Pal buy page a little

--- src/app/php/ehbuy.php
+++ src/app/php/ehbuy.php
@@ -1,18 +1,38 @@
 <html>
 <head><meta http-equiv="Content-Type" content="text/html; charset=us-ascii">
-	<title></title>
+	<title>Psychic Tarot Pay Pal</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 </head>
-<body>
+<body style="background-color: #00f;">
+    
 <div style="width: 80%; margin: auto;">
-<p style="text-align: center;"><span style="font-family:arial,helvetica,sans-serif;"><span style="font-size:36px;">Experimental Site for Pay Pal Demo</span></span></p>
 
-<form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post" target="_top">
-<input type="hidden" name="cmd" value="_s-xclick">
-<input type="hidden" name="hosted_button_id" value="YJ328MCK8NGRU">
-<input type="hidden" name="custom" value="<?php echo $_GET['id']; ?>">
-<input type="image" src="https://www.sandbox.paypal.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-<img alt="" border="0" src="https://www.sandbox.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
-</form>
+    <div class="text-center" style="padding: 20px; color: #fff;">
+        <span style="display: inline-block; vertical-align: middle;">
+            <img id="logo-image-tag" src="http://www.jjtron.com/dist/assets/Logo.png" width="100">
+        </span>
+        <span style="display: inline-block; vertical-align: middle;">
+            <div style="font-size: 30px;" id="title-tag">Psychic Cosmic Tarot Pay-Pal Entry</div>
+        </span>
+        <div class="row" style="padding: 20px; color: #fff;">
+            <div class="col-md-6 offset-md-3" style="font-size: 18px;">
+                <p align="justify">Your psychic reader has looked at your cards. Her full analysis is now ready for you to access. Click on the Pay Pal button below to see your future.</p>
+            </div>
+        </div>
+    </div>
+
+    <div style="text-align: center;">
+        <form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post" target="_top">
+        <input type="hidden" name="cmd" value="_s-xclick">
+        <input type="hidden" name="hosted_button_id" value="YJ328MCK8NGRU">
+        <input type="hidden" name="custom" value="<?php echo $_GET['id']; ?>">
+        <input type="image" src="https://www.sandbox.paypal.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+        <img alt="" border="0" src="https://www.sandbox.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
+        </form>
+    </div>
+    <div class="row" style="text-align: center; padding: 20px; color: #fff;">
+        <div  class="col-md-4 offset-md-4" style="font-size: 16px;">Only 99 &#162;</div>
+    </div>
 
 
 <p style="text-align: left;"></p>
