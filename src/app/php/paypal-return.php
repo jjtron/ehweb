270dbdfdc6080266de10c216415809b3dca6532a
jjtron <gpetron4@cfl.rr.com>
2018-10-31 18:43:01 -0400

    Update to https

--- src/app/php/paypal-return.php
+++ src/app/php/paypal-return.php
@@ -5,7 +5,6 @@
 <title>PayPal Return PHP page</title>
 </head>
 <body>
-    <!-- http://www.jjtron.com/dist/php/paypal-return.html?amt=1.00&cc=USD&item_name=jjtronbutton&st=Completed&tx=2X800551W38090255 -->
 	PayPal Return PHP page
 </body>
 </html>
